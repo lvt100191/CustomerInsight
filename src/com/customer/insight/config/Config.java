4ba1bf40858b38315ae3673596c1722cbb8c619e
lvt100191 <tunglv9x@gmail.com>
2017-09-29 13:19:16 +0700

    t

--- src/com/customer/insight/config/Config.java
+++ src/com/customer/insight/config/Config.java
@@ -16,16 +16,48 @@ import java.util.logging.Logger;
  * @author TUNGLV
  */
 public class Config {
-    public static  String USER_ACCESS_TOKEN = "";
+    static org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(Config.class.getName());
+    public static final String[] CONFIG_LOCATIONS_FILE = new String[]{"config.properties", "config.properties"};
+    private static Properties props = null;
+    private static Config instance = new Config();
+
     static {
         try {
-            Properties prop = new Properties();
-            ClassLoader loader = Thread.currentThread().getContextClassLoader();
-            InputStream stream = loader.getResourceAsStream("config.properties");
-            prop.load(stream);
-            USER_ACCESS_TOKEN = prop.getProperty("USER_ACCESS_TOKEN");
-        } catch (IOException ex) {
-            Logger.getLogger(Config.class.getName()).log(Level.SEVERE, null, ex);
+            props = new Properties();
+            props.load(findResourceAsStream(CONFIG_LOCATIONS_FILE));
+        } catch (Exception e) {
+            logger.error("ERROR Loading config parameters: ", e);
         }
+    }
+    //lay gia tri config trong file config.properties o day
+    public static final String DB_NAME = get("db_name");
+    public static final String USER_ACCESS_TOKEN = get("USER_ACCESS_TOKEN");
+    
+    public static InputStream findResourceAsStream(String[] fileLocations) {
+        for (String fl : fileLocations) {
+            try {
+                InputStream inputStream = getResourceAsStream(fl);
+                if (inputStream != null) {
+                    return inputStream;
+                }
+            } catch (Exception e) {
+                logger.error("Config.findResourceAsStream(): Error: " + e);
+            }
+        }
+        return null;
+    }
+
+    public static InputStream getResourceAsStream(String filePath) {
+        return instance.getClass().getClassLoader().getResourceAsStream(filePath);
+    }
+
+    public static String get(String key) {
+        String value = props.getProperty(key);
+        if (value == null) {
+            logger.error("Get Null config parameter " + key);
+        } else {
+            value = value.trim();
+        }
+        return value;
     }
 }
---
+++ src/com/customer/insight/db/DBUtil.java
@@ -0,0 +1,199 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.customer.insight.db;
+
+import com.customer.insight.config.Config;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+/**
+ *
+ * @author TUNGLV
+ */
+public class DBUtil {
+
+    static Logger logger = Logger.getLogger(DBUtil.class.getName());
+
+    public static void main(String[] args) throws SQLException {
+        //xoa bang
+        //dropTable("TBL_USER");
+                Connection c = null;
+        Statement stmt = null;
+        try {
+            c = connectDB("tunglv.db");
+
+            stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM TBL_USER;");
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String user = rs.getString("username");
+                System.out.println("id = "+id +" user = "+user);
+            }
+            rs.close();
+            stmt.close();
+            c.close();
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("Operation done successfully");
+    }
+
+    //tao ket noi den DB
+    public static Connection connectDB(String fileName) throws SQLException {
+        Connection con = null;
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            con = DriverManager.getConnection("jdbc:sqlite:" + fileName);
+            logger.info("ket noi thanh cong");
+        } catch (Exception e) {
+            logger.error("Loi ket noi den co so du lieu");
+            con.close();
+        }
+        return con;
+    }
+
+    //kiem tra bang du lieu da tao chua
+    //tableName : ten bang
+    //true: bang da ton tai
+    public static boolean tableExists(String tableName) throws SQLException {
+        Connection conn = null;
+        try {
+            conn = connectDB(Config.DB_NAME);
+            DatabaseMetaData md = conn.getMetaData();
+            ResultSet rs = md.getTables(null, null, tableName, null);
+            if (rs.next()) {
+                return true;
+            }
+        } catch (SQLException ex) {
+            logger.error("loi kiem tra bang du lieu");
+        } finally {
+            conn.close();
+        }
+        return false;
+    }
+
+    public static void insertTest() {
+        Connection c = null;
+        Statement stmt = null;
+
+        try {
+
+            c = connectDB("test.db");
+            stmt = c.createStatement();
+            String sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "
+                    + "VALUES (1, 'Paul', 32, 'California', 20000.00 );";
+            stmt.executeUpdate(sql);
+
+            sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "
+                    + "VALUES (2, 'Allen', 25, 'Texas', 15000.00 );";
+            stmt.executeUpdate(sql);
+
+            sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "
+                    + "VALUES (3, 'Teddy', 23, 'Norway', 20000.00 );";
+            stmt.executeUpdate(sql);
+
+            sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "
+                    + "VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 );";
+            stmt.executeUpdate(sql);
+
+            stmt.close();
+            c.commit();
+            c.close();
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("Records created successfully");
+    }
+
+    public static void createTableTest() {
+        Connection c = null;
+        Statement stmt = null;
+
+        try {
+            c = connectDB("test.db");
+
+            stmt = c.createStatement();
+            String sql = "CREATE TABLE COMPANY "
+                    + "(ID INT PRIMARY KEY     NOT NULL,"
+                    + " NAME           TEXT    NOT NULL, "
+                    + " AGE            INT     NOT NULL, "
+                    + " ADDRESS        CHAR(50), "
+                    + " SALARY         REAL)";
+            stmt.executeUpdate(sql);
+            stmt.close();
+            c.close();
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("Table created successfully");
+    }
+
+    public static void selectTableTest() {
+        Connection c = null;
+        Statement stmt = null;
+        try {
+            c = connectDB("test.db");
+
+            stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM COMPANY;");
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                int age = rs.getInt("age");
+                String address = rs.getString("address");
+                float salary = rs.getFloat("salary");
+
+                System.out.println("ID = " + id);
+                System.out.println("NAME = " + name);
+                System.out.println("AGE = " + age);
+                System.out.println("ADDRESS = " + address);
+                System.out.println("SALARY = " + salary);
+                System.out.println();
+            }
+            rs.close();
+            stmt.close();
+            c.close();
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("Operation done successfully");
+    }
+
+    public static void dropTable(String tableName) throws SQLException {
+        Connection c = null;
+        Statement stmt = null;
+        try {
+            c = connectDB("test.db");
+
+            stmt = c.createStatement();
+
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        } finally {
+            stmt.close();
+            c.close();
+        }
+        System.out.println("Operation done successfully");
+    }
+
+}
