35925d147d1b5ae1d0507b8f38918523375e360c
lvt100191 <tunglv9x@gmail.com>
2017-10-02 09:04:10 +0700

    t

--- src/com/customer/insight/facebook/usecase/FanPageAction.java
+++ src/com/customer/insight/facebook/usecase/FanPageAction.java
@@ -11,11 +11,13 @@ import com.customer.insight.facebook.dto.Feed;
 import com.customer.insight.facebook.dto.Page;
 import com.customer.insight.facebook.dto.User;
 import com.customer.insight.http.ResponseUtil;
+import com.customer.insight.mail.EmailAction;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -41,8 +43,52 @@ public class FanPageAction {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         Date fromDate = sdf.parse("2017-10-01");
         ArrayList<Feed> lstFeed = fanPage.getFeed(token, page.getId(), fromDate);
-        //lay danh sach cac binh luan theo tung bai da dang
-        HashMap<String, List<Comment>> commentMap = fanPage.getComments(token,lstFeed);
+        //lay danh sach binh luan theo bai dang
+        String mail = null;
+        for (Feed f : lstFeed) {
+            ArrayList<Comment> comments = fanPage.getComments(token, f.getId());
+            for (Comment c : comments) {
+                //neu co email thi gui mail
+                String comment = c.getContentComment();
+                if (comment.contains("@")) {
+                    Matcher m = Pattern.compile("[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+").matcher(comment);
+                    while (m.find()) {
+                        try{
+                        mail = m.group();
+                        char end = mail.charAt(mail.length() - 1);
+                        if (end == '.') {
+                            mail = mail.substring(0, mail.length() - 1);
+                        }
+                        String from = "lazada.ohaythe@gmail.com";
+                        String pwd = "123456a@";
+                        String title = "Chuột quang không dây Forter V189 V111 + Tặng miếng lót chuột  ";
+                        String content = "http://bit.ly/2hyREyh " + "\n"
+                                + "Là sản phẩm cách tân vừa ra mắt năm 2014 hỗ trợ một cách hoàn hảo công việc văn phòng của bạn, chuột không dây Forter V189 có thiết kể trẻ trung, trang nhã và độ phân giải cao đến 1500dpi. Điểm nổi bật nhất của sản phẩm là khả năng tự động tắt, bật khi kết nối hoặc shutdown máy tính. Đây sẽ là người bạn hỗ trợ đắc lực cho mọi công việc trên máy tính của bạn. Tiếp nối những tính năng nổi trội của các sản phẩm trước, Forter V189 được cải tiến và nâng cấp đáp ứng nhu cầu làm việc với cường độ và áp lực cao.\n"
+                                + "\n"
+                                + "TÍNH NĂNG NỔI BẬT\n"
+                                + "Thiết kế Ergonomic, 3 phím chức năng\n"
+                                + "Nhờ vào thiết kế Ergonomic thông minh, chuột không dây Forter V189 tạo sự thoải mái cho cả người thuận tay phải lẫn tay trái. Sản phẩm được trang bị 3 phím chức năng cơ bản giúp người dùng dễ dàng thao tác, có cảm giác chắc chắn khi di chuyển chuột với cường độ nhanh.\n"
+                                + "\n"
+                                + "Độ phân giải 1500dpi, khoảng cách kết nối 15m\n"
+                                + "Sản phẩm có độ phân giải cao lên đến 1500dpi giúp bạn thao tác nhanh, nhạy trong phạm vi 15m. Vậy nên, dù phải di chuyển liên tục trong văn phòng, bạn vẫn có thể thực hiện công việc trên máy tính một cách nhanh chóng, chính xác.\n"
+                                + "\n"
+                                + "Tự động bật, tắt & tiết kiệm năng lượng\n"
+                                + "Với công nghệ tiên tiến, Forter V189 có khả năng tự động bật, tắt khi máy tính khởi động hoặc shutdown. Điều này giúp tiết kiệm năng lượng tối đa, cao hơn gấp 3 lần so với sản phẩm thông thường trên thị trường. Ngoài ra, sản phẩm được trang bị Pin 1AA Ultra Alkaline có tuổi thọ lên đến 24 tháng.\n"
+                                + "\n"
+                                + "THÔNG TIN SẢN PHẨM\n"
+                                + "Màu: đen\n"
+                                + "Trọng lượng (KG): 0.1\n"
+                                + "Kích thước sản phẩm (D x R x C cm): 10x3x2\n"
+                                + "Bảo hành: 24 tháng- Theo đúng tiêu chuẩn của Nhà sản xuất";
+                        EmailAction.sendEmail(from, pwd, mail, title, content);
+                        }catch(Exception e){
+                            
+                        }
+                    }
+                }
+
+            }
+        }
         System.out.println("thuc hien thanh cong");
     }
 
@@ -102,41 +148,38 @@ public class FanPageAction {
     //ai binh luan
     //noi dung binh luan la gi
     //lstFeed: danh sach bai dang cua trang
-    public HashMap<String,List<Comment>>  getComments(String token, ArrayList<Feed>  lstFeed) throws Exception {
-        HashMap<String,List<Comment>> hashMap = new HashMap<>();
+    public ArrayList<Comment> getComments(String token, String feedId) throws Exception {
+        ArrayList<Comment> listComment = new ArrayList<>();
         JSONParser parser = null;
-        for (int i = 0; i < lstFeed.size(); i++) {
-            String urlGetComment = "https://graph.facebook.com/v2.10/" + lstFeed.get(i).getId() + "/comments?access_token=" + token;
-            String rsComment = ResponseUtil.sendGet(urlGetComment);
-            parser = new JSONParser();
-            JSONObject objComments = (JSONObject) parser.parse(rsComment);
-            JSONArray dataComments = (JSONArray) objComments.get("data");
-            List<Comment> lst = new ArrayList<Comment>();
-            if (dataComments.size() > 0) {
-                for (int j = 0; j < dataComments.size(); j++) {
-                    Comment c = new Comment();
-                    //chu y dataComments get j khong duoc nham bien
-                    JSONObject comment = (JSONObject) dataComments.get(j);
-                    c.setId(comment.get("id").toString());
-                    c.setIdFeed(lstFeed.get(i).getId());
-                    Object user = comment.get("from");
-                    if (user != null) {
-                        String userJson = comment.get("from").toString();
-                        parser = new JSONParser();
-                        JSONObject objUser = (JSONObject) parser.parse(userJson);
-                        User u = new User();
-                        u.setId(objUser.get("id").toString());
-                        u.setName(objUser.get("name").toString());
-                        c.setUser(u);
-                    }
-                    c.setTimeComment(comment.get("created_time").toString());
-                    c.setContentComment(comment.get("message").toString());
-                    //trong lst chua danh sach nguoi binh luan userName
-                    lst.add(c);
+        String urlGetComment = "https://graph.facebook.com/v2.10/" + feedId + "/comments?access_token=" + token;
+        String rsComment = ResponseUtil.sendGet(urlGetComment);
+        parser = new JSONParser();
+        JSONObject objComments = (JSONObject) parser.parse(rsComment);
+        JSONArray dataComments = (JSONArray) objComments.get("data");
+        List<Comment> lst = new ArrayList<Comment>();
+        if (dataComments!=null && dataComments.size() > 0) {
+            for (int j = 0; j < dataComments.size(); j++) {
+                Comment c = new Comment();
+                //chu y dataComments get j khong duoc nham bien
+                JSONObject comment = (JSONObject) dataComments.get(j);
+                c.setId(comment.get("id").toString());
+                c.setIdFeed(feedId);
+                Object user = comment.get("from");
+                if (user != null) {
+                    String userJson = comment.get("from").toString();
+                    parser = new JSONParser();
+                    JSONObject objUser = (JSONObject) parser.parse(userJson);
+                    User u = new User();
+                    u.setId(objUser.get("id").toString());
+                    u.setName(objUser.get("name").toString());
+                    c.setUser(u);
                 }
-             hashMap.put(lstFeed.get(i).getId(), lst);
+                c.setTimeComment(comment.get("created_time").toString());
+                c.setContentComment(comment.get("message").toString());
+                //trong lst chua danh sach nguoi binh luan userName
+                listComment.add(c);
             }
         }
-        return hashMap;
+        return listComment;
     }
 }
---
+++ src/com/customer/insight/mail/EmailAction.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.customer.insight.mail;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Properties;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+/**
+ *
+ * @author TUNGLV
+ */
+public class EmailAction {
+
+    public static void main(String[] args) {
+        String from = "lazada.ohaythe@gmail.com";
+        String pwd = "123456a@";
+        String title = "Chuột quang không dây Forter V189 V111 + Tặng miếng lót chuột  ";
+        String content = "http://bit.ly/2hyREyh "
+                + "Là sản phẩm cách tân vừa ra mắt năm 2014 hỗ trợ một cách hoàn hảo công việc văn phòng của bạn, chuột không dây Forter V189 có thiết kể trẻ trung, trang nhã và độ phân giải cao đến 1500dpi. Điểm nổi bật nhất của sản phẩm là khả năng tự động tắt, bật khi kết nối hoặc shutdown máy tính. Đây sẽ là người bạn hỗ trợ đắc lực cho mọi công việc trên máy tính của bạn. Tiếp nối những tính năng nổi trội của các sản phẩm trước, Forter V189 được cải tiến và nâng cấp đáp ứng nhu cầu làm việc với cường độ và áp lực cao.\n"
+                + "\n"
+                + "TÍNH NĂNG NỔI BẬT\n"
+                + "Thiết kế Ergonomic, 3 phím chức năng\n"
+                + "Nhờ vào thiết kế Ergonomic thông minh, chuột không dây Forter V189 tạo sự thoải mái cho cả người thuận tay phải lẫn tay trái. Sản phẩm được trang bị 3 phím chức năng cơ bản giúp người dùng dễ dàng thao tác, có cảm giác chắc chắn khi di chuyển chuột với cường độ nhanh.\n"
+                + "\n"
+                + "Độ phân giải 1500dpi, khoảng cách kết nối 15m\n"
+                + "Sản phẩm có độ phân giải cao lên đến 1500dpi giúp bạn thao tác nhanh, nhạy trong phạm vi 15m. Vậy nên, dù phải di chuyển liên tục trong văn phòng, bạn vẫn có thể thực hiện công việc trên máy tính một cách nhanh chóng, chính xác.\n"
+                + "\n"
+                + "Tự động bật, tắt & tiết kiệm năng lượng\n"
+                + "Với công nghệ tiên tiến, Forter V189 có khả năng tự động bật, tắt khi máy tính khởi động hoặc shutdown. Điều này giúp tiết kiệm năng lượng tối đa, cao hơn gấp 3 lần so với sản phẩm thông thường trên thị trường. Ngoài ra, sản phẩm được trang bị Pin 1AA Ultra Alkaline có tuổi thọ lên đến 24 tháng.\n"
+                + "\n"
+                + "THÔNG TIN SẢN PHẨM\n"
+                + "Màu: đen\n"
+                + "Trọng lượng (KG): 0.1\n"
+                + "Kích thước sản phẩm (D x R x C cm): 10x3x2\n"
+                + "Bảo hành: 24 tháng- Theo đúng tiêu chuẩn của Nhà sản xuất";
+        ArrayList<String> lst = null;
+        for (String to : lst) {
+            try {
+                sendEmail(from, pwd, to, title, content);
+                System.out.println("tunglv gui toi mail" + to + " thanh cong");
+            } catch (Exception e) {
+                System.out.println("tunglv gui toi mail: " + to + " bi loi" + e.getMessage());
+            }
+        }
+    }
+
+    //mailSend: email gui
+    //passwordMailSend: mat khau cua email gui
+    //mailRecipient: dia chi email nhan
+    //title: tieu de mail
+    //content: noi dung mail
+    public static void sendEmail(String mailSend, String passwordMailSend, String mailRecipient, String title, String content) throws MessagingException {
+        final String SSL_FACTORY = "javax.net.ssl.SSLSocketFactory";
+        // Get a Properties object
+        Properties props = System.getProperties();
+        props.setProperty("mail.smtp.host", "smtp.gmail.com");
+        props.setProperty("mail.smtp.socketFactory.class", SSL_FACTORY);
+        props.setProperty("mail.smtp.socketFactory.fallback", "false");
+        props.setProperty("mail.smtp.port", "465");
+        props.setProperty("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.debug", "true");
+        props.put("mail.store.protocol", "pop3");
+        props.put("mail.transport.protocol", "smtp");
+        try {
+            Session session = Session.getDefaultInstance(props,
+                    new Authenticator() {
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    //chu y neu gap loi nay thi phai bat bao mat tren tai khoan mail google cho phep ung dung it bao mat hon Erreur d'envoi, cause: javax.mail.AuthenticationFailedException: 535-5.7.8 Username and Password not accepted. Learn more at
+                    return new PasswordAuthentication(mailSend, passwordMailSend);
+                }
+            });
+
+            // -- Create a new message --
+            Message msg = new MimeMessage(session);
+
+            // -- Set the FROM and TO fields --
+            msg.setFrom(new InternetAddress(mailSend));
+            msg.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(mailRecipient, false));
+            msg.setSubject(title);
+            msg.setText(content);
+            msg.setSentDate(new Date());
+            Transport.send(msg);
+            //System.out.println("Message sent.");
+        } catch (MessagingException e) {
+            throw new MessagingException(e.getMessage());
+        }
+    }
+}
